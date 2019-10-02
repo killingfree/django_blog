915df973909f338e80e4c277af491c851454033e
gr1g@mail.ru <byltynbabrfnjh222>
2019-10-02 14:48:23 +0300

    add function shared post

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,11 +3,18 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
 
-from .models import Post
-from .forms import EmailPostForm
+from .models import Post, Comment
+from .forms import EmailPostForm, CommentForm
+from taggit.models import Tag
+from django.db.models import Count
 
-def post_list(request):
+
+def post_list(request, tag_slug=None):
     object_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug= tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
     paginator = Paginator(object_list, 3)  # 3 публикации на странице
     page = request.GET.get('page')
     try:
@@ -18,16 +25,44 @@ def post_list(request):
     except EmptyPage:
         # Если номер страницы больше, чем общее количество страниц, возвращаем последнюю.
         posts = paginator.page(paginator.num_pages)
+
     return render(request,
                   'blog/post/list.html',
                   {'page': page,
-                   'posts': posts})
+                   'posts': posts,
+                   'tag': tag})
 
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published', publish__year=year,
                              publish__month=month, publish__day=day)
-    return render(request, 'blog/post/detail.html', {'post': post})
+    # Список активных комментариев для этой статьи
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    if request.method == 'POST':
+        # пользователь оставил кооментарий
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # создаем комментарий но пока не сохраняем в базе данных
+            new_comment = comment_form.save(commit=False)
+            # привязываем комментарий к текущей статье
+            new_comment.post = post
+            # сохраняем коментарий в бд
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in = post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by(
+        '-same_tags','-publish'
+    )[:4]
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post,
+                   'comments': comments,
+                   'new_comment': new_comment,
+                   'comment_form': comment_form,
+                   'similar_posts': similar_posts})
 
 
 class PostListView(ListView):
